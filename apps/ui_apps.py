17ef7b43226a6725edfcf8799835724f92ce2590
TalalAzdeen <azadeentalal774355690@gmail.com>
2025-03-18 03:23:56 +0300

    Update ui_apps.py

--- apps/ui_apps.py
+++ apps/ui_apps.py
@@ -2,7 +2,7 @@ from AppUI import create_app
 from .chatbot.templates  import TemplateSpeechStudioBuilder
 from .space.templates  import TemplateSpacePlatformBuilder
 from .t2text.templates  import TemplateTextToTextStudioBuilder
-from .t2speech.templates  import TemplateTextToSpeechStudioBuilder
+from .t2speech.templates  import TemplateTextToSpeechStudioBuilder,TemplateTextToSpeechBuilder
 isDev=False
 
 APPS=[
@@ -10,6 +10,7 @@ APPS=[
       (create_app(TemplateTextToSpeechStudioBuilder,isDev),'studio-t2text'),
       (create_app(TemplateSpacePlatformBuilder,isDev),'createspace'),
       (create_app(TemplateTextToTextStudioBuilder,isDev),'studio-t2speech'),
+       (create_app(TemplateTextToSpeechBuilder,isDev),'t2speech')
 
       
 ]
