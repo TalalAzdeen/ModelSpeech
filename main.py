c4490cde9905af6e45fcbeb64923bbe7ef2695b4
TalalAzdeen <azadeentalal774355690@gmail.com>
2025-03-21 02:50:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,10 +44,9 @@ async def redirect_to_site():
 
 from apps.ui_apps import APPS
 for uiapp,path in APPS:
-    #app = gr.mount_gradio_app(app, uiapp, path="/"+path)
-    uiapp.launch(show_error=True,share=True)
+    app = gr.mount_gradio_app(app, uiapp, path="/"+path)
+    #uiapp.launch(show_error=True,share=True)
 
-    
-# from apps.api_routers import APIS
-# for router,path in  APIS:
-#     app.include_router(router, prefix=f"/api/{path}")
+from apps.api_routers import APIS
+for router,path in  APIS:
+     app.include_router(router, prefix=f"/api/{path}")
