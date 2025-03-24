0ad9739aea6cd4d07d3b39fa077af0decc79ccf1
TalalAzdeen <azadeentalal774355690@gmail.com>
2025-03-25 02:22:45 +0300

    Update routers.py

--- apps/chatbot/routers.py
+++ apps/chatbot/routers.py
@@ -72,7 +72,7 @@ class UserHandler:
         def error_event_handler(e: Exception, function_name: str = ""):
             return self.__builder.error_event_handler(e, function_name)
         @self.router.post("/handle_error")
-        def error_event_handler(message: str, status_code: int):
+        def handle_error(message: str, status_code: int):
             return self.__builder.handle_error(message, status_code)
 
     def get_router(self):
