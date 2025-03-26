86dedbc709544f4b7a87685e5b6a7e22097c16ef
TalalAzdeen <azadeentalal774355690@gmail.com>
2025-03-26 03:42:54 +0300

    Update api_routers.py

--- apps/api_routers.py
+++ apps/api_routers.py
@@ -4,6 +4,6 @@ from .chatbot.templates  import TemplateSpeechStudioBuilder
 from .space.templates  import TemplateSpacePlatformBuilder
 isDev=True
 APIS=[
-  (create_api(TemplateSpeechStudioBuilder,isDev),'/user')
+  (create_api(TemplateSpeechStudioBuilder,isDev),'/user'),
   (create_api(TemplateSpacePlatformBuilder,isDev),'/spaceapi')
 ]
