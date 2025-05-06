a95007b64aa740fd5c24a2b38f993817f89c0c87
TalalAzdeen <azadeentalal774355690@gmail.com>
2025-05-06 23:42:11 +0300

    Update templates.py

--- apps/chatbot/templates.py
+++ apps/chatbot/templates.py
@@ -144,7 +144,7 @@ class TemplateSpeechStudioBuilder:
     def send_request(self,data):
     
         try:
-            request = self.builderRequest.send_create_request_quary(data)
+            request = self.builderRequest.send_create_request_quary(data,"chatbot")
             return request
         except Exception as e:
             
