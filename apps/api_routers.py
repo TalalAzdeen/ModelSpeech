eabcb328e4b45cd082d5ab31794d8dd8b47e8917
TalalAzdeen <azadeentalal774355690@gmail.com>
2025-05-21 16:48:25 +0300

    Update api_routers.py

--- apps/api_routers.py
+++ apps/api_routers.py
@@ -1,7 +1,9 @@
 from AppUI import create_api
-from .t2text.templates  import TemplateTextToTextStudioBuilder,TemplateTextToTextBuilder
 from .chatbot.templates  import TemplateSpeechStudioBuilder
 from .space.templates  import TemplateSpacePlatformBuilder
+from .dashboard.templates  import TamplateDashBuilder
+from .t2text.templates  import TemplateTextToTextStudioBuilder,TemplateTextToTextBuilder
+from .t2speech.templates  import TemplateTextToSpeechStudioBuilder,TemplateTextToSpeechBuilder
 isDev=True
 APIS=[
   (create_api(TemplateSpeechStudioBuilder,isDev),'/user')
