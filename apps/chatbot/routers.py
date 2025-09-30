656aef75e11ab0cc7b33f6718b53428c5106fa48
TalalAzdeen <azadeentalal774355690@gmail.com>
2025-10-01 00:16:12 +0300

    Update routers.py

--- apps/chatbot/routers.py
+++ apps/chatbot/routers.py
@@ -15,8 +15,8 @@ class UserHandler:
         def Chat_Text2Text(message: str, key: str = ""):
             return self.__builder.ask_ai(message,key)
         @self.router.post("/ChatText2Speech")
-        def Chat_Text2Speech(text: str,voice:str="alloy", speed=1,api_key: str = ""):
-            return self.__builder.text_to_speech(text,voice,speed,api_key)
+        def Chat_Text2Speech(text:str,api_key: str,file_type:str="wav",voice:str="alloy", speed=1):
+            return self.__builder.text_to_speech_and_upload(text,api_key,file_type,voice,speed)
        
         
     def get_router(self):
