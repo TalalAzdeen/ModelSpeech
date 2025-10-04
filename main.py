d6dc1bc7b851ff9382c27785f93cb4526658bf4d
TalalAzdeen <azadeentalal774355690@gmail.com>
2025-10-04 17:05:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ for uiapp,path in APPS:
 from apps.api_routers import APIS
 for router,path in  APIS:
      app.include_router(router, prefix=f"/api/{path}")
-app.include_router(get_router(), prefix="/encrypt")
+ 
